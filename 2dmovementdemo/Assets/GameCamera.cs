e287843146be98a2ad0b62ccb23fa8312ef10c28
KyleANepo <kyle.nepo@wsu.edu>
2024-10-02 02:43:56 -0700

    smooth camera ig
    smooth camera ig

---
+++ 2dmovementdemo/Assets/GameCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 locationOffset;
+    // public Vector3 rotationOffset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + Quaternion.identity * locationOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
